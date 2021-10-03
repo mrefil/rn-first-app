aec69cfa55f1abbc76d79425fb30a9d8d246a9a8
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-10-03 09:57:21 +0300

    FlexBox Deep Dive

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  // const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1}} />
-        <Button title="ADD" />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
