4934a7a271eae0d56b80395c71b199f068ae474d
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-10-03 08:07:15 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+  // const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change text" onPress={() => setOutputText('Text Changed!')} />
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1}} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
