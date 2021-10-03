d2125bec4f7c7128848231e14e1fdf7bc70f5f91
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-10-03 10:12:51 +0300

    inline style

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   // const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1, width: '80%'}} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View>
@@ -18,5 +18,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black', 
+    borderWidth: 1, 
+    width: '80%'
+  }
 });
