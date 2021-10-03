96baf010c2d9f2c0b62152c91320546125222eeb
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-10-03 10:07:41 +0300

    back to the main app

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  // const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={{ padding: 50, flexDirection: 'row', width: '80%', height: 300, justifyContent: 'space-around', alignItems: 'stretch' }}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{borderBottomColor: 'black', borderWidth: 1, width: '80%'}} />
+        <Button title="ADD" />
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+      <View>
+
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+
+});
